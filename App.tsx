fbcfe96d1d59637eaa09159d9a5a422df39964ca
Luke Warner <lukeswarner@gmail.com>
2024-03-09 17:11:48 -0700

    raise overlay on press

--- App.tsx
+++ App.tsx
@@ -1,28 +1,45 @@
+import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { Pressable, Text, View } from 'react-native';
 import YoutubePlayer from 'react-native-youtube-iframe';
 
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 
 
 export default function App() {
+  const [isPlaying, setIsPlaying] = useState<boolean>(true)
+  const [showOverlay, setShowOverlay] = useState<boolean>(false)
+
+
   return (
-    <View className='flex-1 justify-center bg-black'>
-      <Text className='text-sky-400'>Open up App.js to start working on your app!</Text>
+    <View className='absolute bg-green-400 w-full'>
       <Pressable
         onPress={() => {
-          // handle or ignore
+          setShowOverlay(!showOverlay)
         }}
         onLongPress={() => {
           // handle or ignore
         }}>
 
-        <View pointerEvents="none">
+        <View pointerEvents="none" className='w-full'>
             <YoutubePlayer
-              height={300}
+              height={340}
               videoId='M14pPBqfkLU'
-              play
+              play={isPlaying}
             />
         </View>
+        {showOverlay && 
+        <View className='bottom-60 flex flex-row justify-center'>
+          <Pressable onPress={()=>setIsPlaying(!isPlaying)}>
+            <FontAwesome name="play" size={64} color={'#ffffff'} />
+          </Pressable>
+            
+         
+          
+     
+        </View>
+        }
         </Pressable>
       <StatusBar style="auto" />
     </View>
